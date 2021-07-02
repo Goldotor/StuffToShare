0fed6089dd9a3ab5a621f7ee278fe12a161fc229
Goldotor <86830238+Goldotor@users.noreply.github.com>
2021-07-02 11:25:36 +0100

    Add files via upload

---
+++ checkpoint2/checkpoint2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace checkpoint2
+{
+    class Program
+    {
+       static double Sum(double a,double b)
+        {
+            return (a + b);
+
+        }
+        static double Prod(double a, double b)
+        {
+            return (a * b);
+
+        }
+        static double Div(double a, double b)
+        {
+            return (a / b);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
